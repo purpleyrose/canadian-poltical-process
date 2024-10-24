2601cc3080273937322c4a30592453039c5d0dcb
llilarose <99583971+purpleyrose@users.noreply.github.com>
2024-10-24 18:20:43 -0400

    Changed from Godot Script to C#. Added more content to the Office->Legislation tab

---
+++ Main.cs
@@ -0,0 +1,39 @@
+using Godot;
+using System;
+
+public partial class Main : Node2D
+{
+	private const int StartingYear = 2020;
+	private const int StartingWeek = 1;
+	private Label PoliticalPointsLabel;
+	private Label DateLabel;
+	public int PoliticalPoints = 0;
+	private Button NextWeekButton;
+	private int CurrentWeek = StartingWeek;
+	private int CurrentYear = StartingYear;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		NextWeekButton = GetNode<Button>("Panel/NextWeekButton");
+		NextWeekButton.Connect("pressed", new Callable(this, "_on_next_week_button_pressed"));
+		DateLabel = GetNode<Label>("Panel/DateLabel");
+		DateLabel.Text = "Year " + StartingYear + ", Week " + StartingWeek;
+
+		PoliticalPointsLabel = GetNode<Label>("Panel/PoliticalPointsLabel");
+		PoliticalPointsLabel.Text = "Political Points: " + PoliticalPoints;
+	}
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public void _on_next_week_button_pressed()
+	{
+		CurrentWeek++;
+		if (CurrentWeek > 52)
+		{
+			CurrentWeek = 1;
+			CurrentYear++;
+		}
+		DateLabel.Text = "Year " + CurrentYear + ", Week " + CurrentWeek;
+	}
+	public override void _Process(double delta)
+	{
+	}
+}
---
+++ SplashScreen.cs
@@ -0,0 +1,20 @@
+using Godot;
+using System;
+
+public partial class SplashScreen : Node2D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+	public void _on_start_game_button_pressed()
+	{
+		GetTree().ChangeSceneToFile("res://Main.tscn"); // Change scene to Main.tscn
+	}
+
+	public void _on_quit_game_button_pressed()
+	{
+		GetTree().Quit(); // Quit the game
+	}
+	
+}
