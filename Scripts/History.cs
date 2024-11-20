6e56e426a1769a1bbb59e343a4de753f09688cf4
llilarose <99583971+purpleyrose@users.noreply.github.com>
2024-11-20 13:35:25 -0500

    Add error handling to History feature; implement validation for year inputs and title in AddHistoryPanel

--- Scripts/History.cs
+++ Scripts/History.cs
@@ -8,6 +8,7 @@ public partial class History : Control
 	private LineEdit HistoryTitleLineEdit; // LineEdit for the title
 	private SpinBox StartYearSpinBox;
 	private SpinBox EndYearSpinBox;
+	private Label ErrorLabel;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -17,33 +18,51 @@ public partial class History : Control
 		StartYearSpinBox = GetNode<SpinBox>("AddHistoryPanel/StartYearBox");
 		EndYearSpinBox = GetNode<SpinBox>("AddHistoryPanel/EndYearSpinBox");
 		HistoryTitleLineEdit = GetNode<LineEdit>("AddHistoryPanel/HistoryTitle");
-
+		ErrorLabel = GetNode<Label>("AddHistoryPanel/Error Label");
 	}
 
 	public void _on_add_history_button_pressed()
 	{
 		AddHistoryPopupPanel.Visible = true;
 	}
+	public void ResetForm()
+	{
+		HistoryTitleLineEdit.Text = "";
+		StartYearSpinBox.Value = 0;
+		EndYearSpinBox.Value = 0;
+		ErrorLabel.Text = "";
+	}
 	
 	public void _on_add_button_pressed()
 	{
+		
+		if (StartYearSpinBox.Value == 0 && EndYearSpinBox.Value == 0)
+		{
+			ErrorLabel.Text = "End year and start year must be set";
+			return ;
+		}
+		else if (StartYearSpinBox.Value > EndYearSpinBox.Value)
+		{
+			ErrorLabel.Text = "Start year must be less than end year";
+			return ;
+		}
+		else if (HistoryTitleLineEdit.Text.Trim() == "")
+		{
+			ErrorLabel.Text = "Title must be set";
+			return ;
+		}
 		AddHistoryPopupPanel.Visible = false;
 		// Add history to the history list by creating an Hbox with the title and years
 		HBoxContainer historyHBox = new HBoxContainer();
 		Label historyTitle = new Label();
 		historyTitle.Text = HistoryTitleLineEdit.Text;
 		Label historyYears = new Label();
-		if (StartYearSpinBox.Value == 0 && EndYearSpinBox.Value == 0)
-		{
-			Label ErrorLabel = new Label();
-			ErrorLabel.Text = "End year and start year must be set";
-			AddHistoryPopupPanel.AddChild(ErrorLabel);
-			return ; // Don't add history if the years are not set
-		}
+		
 		historyYears.Text = StartYearSpinBox.Value + " - " + EndYearSpinBox.Value;
 		historyHBox.AddChild(historyTitle);
 		historyHBox.AddChild(historyYears);
 		GetNode<VBoxContainer>("HistoryListBox").AddChild(historyHBox);
+		ResetForm();
 	}
 
 	public void _on_exit_button_pressed()
